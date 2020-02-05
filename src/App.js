5ce089469213f7a14e0363c9d31e469f8b80f445
vesono <rbrb-sry229@outlook.com>
2020-02-05 13:33:53 +0900

    add s3 upload process, modify readme

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { withAuthenticator, PhotoPicker } from 'aws-amplify-react';
+import Amplify, { Storage } from 'aws-amplify';
+import awsmobile from './aws-exports';
 import './App.css';
 
-function App() {
+Amplify.configure(awsmobile);
+
+// const Header = () => {
+// 
+// }
+
+// file upload処理
+const ImageFileUpload = async (key, object, type) => {
+  try {
+      const result = await Storage.put(key, object, type);
+      return result;
+  } catch (err) {
+      throw err;
+  }
+}
+
+// main処理
+const App = () => {
+
+  const onPickEvent = data => {
+    const { file } = data;
+    const option = { 
+      level: 'private',
+      contentType: file.type
+    };
+    ImageFileUpload(file.name, file, option).then((result) => {
+      // google apiを呼び出す
+      console.log(result);
+    })
+    ImageFileUpload(file.name, file, option).catch((err) => {
+      console.log(err);
+    })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <PhotoPicker preview onPick={data => onPickEvent(data)} />
   );
 }
 
-export default App;
+// export default App;
+export default withAuthenticator(App);

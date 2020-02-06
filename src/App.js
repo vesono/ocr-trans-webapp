14e799e6981bc4c545fe7a9340c7e95b89666005
vesono <rbrb-sry229@outlook.com>
2020-02-06 11:36:22 +0900

    add google cloud vision api process

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import awsmobile from './aws-exports';
 import './App.css';
 import { Header } from './Header'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import gcp_env from './gcp_config/api_gcp_config.js';
 
 Amplify.configure(awsmobile);
 
@@ -15,19 +16,53 @@ export const logout = () => {
     .catch(err => console.log(err));
 }
 
-// file upload処理
+// file upload処理関数
 const ImageFileUpload = async (key, object, type) => {
   try {
       const result = await Storage.put(key, object, type);
       return result;
   } catch (err) {
-      throw err;
+    console.log(err);
   }
+}
+
+// google cloud vision api呼び出し関数
+const SendCloudVison = async dataurl => {
+  const datasplit = dataurl.split( ',' );
+  const data64 = datasplit[1];
+  console.log(data64);
+  // json body作成
+  const body = JSON.stringify({
+    requests: [
+      {
+        features: [{ type: "TEXT_DETECTION", maxResults: 1 }],
+        image: {
+          content: data64
+        }
+      }
+    ]
+  });
+  // cloud vision api呼び出し
+  const response = await fetch(
+    "https://vision.googleapis.com/v1/images:annotate?key=" +
+    gcp_env["GOOGLE_CLOUD_VISION_API_KEY"],
+    {
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json"
+      },
+      method: "POST",
+      body: body
+    }
+  );
+  const resJson = await response.json();
+  console.log(resJson);
 }
 
 // main処理
 export const Home = () => {
 
+  // 保存処理イベント
   const onPickEvent = data => {
     const { file } = data;
     const option = { 
@@ -38,15 +73,13 @@ export const Home = () => {
       // google apiを呼び出す
       console.log(result);
     })
-    ImageFileUpload(file.name, file, option).catch((err) => {
-      console.log(err);
-    })
   }
 
   return (
     <div>
       <Header />
-      <PhotoPicker preview onPick={data => onPickEvent(data)} />
+      <PhotoPicker preview onPick={data => onPickEvent(data)}
+                           onLoad={dataURL => SendCloudVison(dataURL)} />
     </div>
   );
 }
